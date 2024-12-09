95100eae7666493e43f4c0e35ccbf1d7cdff7210
Shubh Tiwari <shubhtiwari9569@gmail.com>
2024-12-09 15:52:30 +0530

    Github provider integrated

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  env:{
+    GITHUB_APP_CLIENT_ID:"Iv23liEMr9zx8ALWsYcf",
+    GITHUB_APP_CLIENT_SECRET:"454421614b9c3c82c2db3a0897fc8cee353a1ddb",
+    NEXTAUTH_SECRET:"B9Po59ERXyF2YUcSv8cEvRQhEXQcPUuNcGf2bzRIu28="
+  }
 };
 
 export default nextConfig;
